ce79f54a40de2d67a37d0fa66ce4abd791e5effb
rifat98pro <rifatahammed01902755@gmail.com>
2026-05-29 12:55:37 +0600

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="talentapp:project_verification" content="674521d3031f041cb7b7ce2e25042b7a7997c6f1c9adcf736a8a322d0064a629eb42badf4cd39c0c2b2b88cb86d8a4e118c83bf0eacc0db6844588745b035735" />
+      </head>
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
       </body>
